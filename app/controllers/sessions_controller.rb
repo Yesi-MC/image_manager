c9d8b199334841ae3bfed79c65e5244fc3da0bc9
Yesi Meza <yesi.meza10@gmail.com>
2021-09-21 20:17:08 -0600

    created controllers

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def login
+    user = User.find_by(email: params[:email].downcase)
+    if user && user.authenticate(params[:password])
+       session[:user_id] = user.id 
+       flash[:success] = "Welcome to your Dashboard, #{user.name}"
+
+       redirect_to dashboard_path(user)
+    else 
+      flash[:error] = "Invalid Credentials, please try again"
+      redirect_to root_path
+    end
+  end
+
+  def logout 
+    session.delete(:user_id)
+    flash[:notice] = "You have been logged out"
+    redirect_to root_path
+  end
+end
