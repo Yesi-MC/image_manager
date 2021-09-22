9cfafefddd176ce2c87b0e42ff82d74bffe4d0b5
Yesi Meza <yesi.meza10@gmail.com>
2021-09-21 21:13:36 -0600

    added new route to image

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new]
     get '/users/:id/dashboard', to: 'users/dashboard#index', as: :dashboard
+    get '/users/:id/image/new', to: 'users/images#new', as: :new_user_image
 end
