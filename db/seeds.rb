2788501a2706434ce4bb92f09bff424d3839bc52
Yesi Meza <yesi.meza10@gmail.com>
2021-09-21 20:16:15 -0600

    added a user to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create!(name: "Joe User", email: "joeuser@gmail.com", password: "!pass123!")
